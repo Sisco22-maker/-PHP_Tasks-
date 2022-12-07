d5eb68a0aa5794a0a582482221ab8235f6548cc7
IslaMSobhyYousof <64289312+Sisco22-maker@users.noreply.github.com>
2022-12-07 11:18:23 +0200

    Add files via upload

---
+++ 6#Sixth Task/1#task number one.php
@@ -0,0 +1,6 @@
+<?php
+$index = 10;
+for (; $index > 0; $index--)
+    echo $index . "<br>";
+?>
+
---
+++ 6#Sixth Task/6#task number six.php
@@ -0,0 +1,8 @@
+<?php
+$money = ["Ahmed" => 100, "Sayed" => 150, "Osama" => 100, "Maher" => 250];
+foreach($money as $key => $value)
+{
+    echo "The Name Is " . $key . " And I Need " . $value . " Pound From Him" . "<br>";
+}
+?>
+
