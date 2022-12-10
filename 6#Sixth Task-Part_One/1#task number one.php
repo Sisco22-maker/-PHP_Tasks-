24b540b918cbfa1a60721f3918b391a9ecfcf8de
IslaMSobhyYousof <64289312+Sisco22-maker@users.noreply.github.com>
2022-12-10 11:11:20 +0200

    Delete 6#Sixth Task-Part_One directory

--- 6#Sixth Task-Part_One/1#task number one.php
+++
@@ -1,6 +0,0 @@
-<?php
-$index = 10;
-for (; $index > 0; $index--)
-    echo $index . "<br>";
-?>
-
--- 6#Sixth Task-Part_One/6#task number six.php
+++
@@ -1,8 +0,0 @@
-<?php
-$money = ["Ahmed" => 100, "Sayed" => 150, "Osama" => 100, "Maher" => 250];
-foreach($money as $key => $value)
-{
-    echo "The Name Is " . $key . " And I Need " . $value . " Pound From Him" . "<br>";
-}
-?>
-
